18766836c3164cd01e2cd84fc2428313427fd65f
Arpan Dutta <smalldutta@gmail.com>
2021-04-26 14:57:09 +0300

    modifies scaffolding

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,22 @@ import 'dart:io' show Platform;
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Platform.isAndroid
-              ? Switch(value: true, onChanged: (v) => null)
-              : CupertinoSwitch(value: true, onChanged: (v) => null),
-        ),
+        appBar: AppBar(title: Text('Hello World'),),
+        body: Center(child: Icon(Icons.cake),),
+        floatingActionButton: FloatingActionButton(onPressed: () {},),
+        drawer: Drawer(),
+        bottomNavigationBar: BottomNavigationBar(items: [
+          BottomNavigationBarItem(label: 'foo', icon: Icon(Icons.call)),
+          BottomNavigationBarItem(label: 'bar', icon: Icon(Icons.cached))
+        ]),
       ),
     );
+
   }
 }

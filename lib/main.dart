057cc427a30cae084e247e8af1b22f47613da321
Arpan Dutta <smalldutta@gmail.com>
2021-05-10 14:44:23 +0300

    adds auth service using Firebase and store, using future and streams, with multi-provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:provider/provider.dart';
+import 'package:quizapp/services/auth.dart';
 
 import 'screens/screens.dart';
 
@@ -9,35 +12,40 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Firebase Analytics
-      navigatorObservers: [
-        FirebaseAnalyticsObserver(analytics: FirebaseAnalytics()),
+    return MultiProvider(
+      providers: [
+        StreamProvider<FirebaseUser>.value(value: AuthService().user)
       ],
+        child: MaterialApp(
+        // Firebase Analytics
+        navigatorObservers: [
+          FirebaseAnalyticsObserver(analytics: FirebaseAnalytics()),
+        ],
 
-      routes: {
-        '/': (context) => LoginScreen(),
-        '/topics': (context) => TopicsScreen(),
-        '/profile': (context) => ProfileScreen(),
-        '/about': (context) => AboutScreen(),
-      },
+        routes: {
+          '/': (context) => LoginScreen(),
+          '/topics': (context) => TopicsScreen(),
+          '/profile': (context) => ProfileScreen(),
+          '/about': (context) => AboutScreen(),
+        },
 
-      // Theme
-      theme: ThemeData(
-        // customizations here
-        fontFamily: 'Nunito',
-        bottomAppBarTheme: BottomAppBarTheme(
-          color: Colors.black87,
+        // Theme
+        theme: ThemeData(
+          // customizations here
+          fontFamily: 'Nunito',
+          bottomAppBarTheme: BottomAppBarTheme(
+            color: Colors.black87,
+          ),
+          brightness: Brightness.dark,
+          textTheme: TextTheme(
+            body1: TextStyle(fontSize: 18),
+            body2: TextStyle(fontSize: 16),
+            button: TextStyle(letterSpacing: 1.5, fontWeight: FontWeight.bold),
+            headline: TextStyle(fontWeight: FontWeight.bold),
+            subhead: TextStyle(color: Colors.grey),
+          ),
         ),
-        brightness: Brightness.dark,
-        textTheme: TextTheme(
-          body1: TextStyle(fontSize: 18),
-          body2: TextStyle(fontSize: 16),
-          button: TextStyle(letterSpacing: 1.5, fontWeight: FontWeight.bold),
-          headline: TextStyle(fontWeight: FontWeight.bold),
-          subhead: TextStyle(color: Colors.grey),
-        ),
-      ),
+      )
     );
   }
 }

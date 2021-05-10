e1a2294d56412fb2a4f84e2c659f8b5f37964fbe
Arpan Dutta <smalldutta@gmail.com>
2021-05-10 12:58:13 +0300

    sets up initial routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'dart:io' show Platform;
-import 'dart:math';
+import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
 void main() => runApp(MyApp());
-
-
-
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      // Firebase Analytics
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: FirebaseAnalytics()),
+      ],
 
       routes: {
-        '/home': (context) => HomeScreen(),
-        '/slideshow': (context) => SlideshowScreen()
+        '/': (context) => LoginScreen(),
+        '/topics': (context) => TopicsScreen(),
+        '/profile': (context) => ProfileScreen(),
+        '/about': (context) => AboutScreen(),
       },
 
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Home'), backgroundColor: Colors.red,),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FlatButton(child: Text('push'), color: Colors.green, onPressed: () {
-
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(builder: (context) => SlideshowScreen(name: 'Jeff'))
-              // );
-
-
-              //  Navigator.pushNamed(
-              //     context,
-              //     '/slideshow'
-              //   );
-
-              Navigator.pushNamed(
-                  context,
-                  '/slideshow'
-              );
-
-            },),
-
-          ],
+      // Theme
+      theme: ThemeData(
+        // customizations here
+        fontFamily: 'Nunito',
+        bottomAppBarTheme: BottomAppBarTheme(
+          color: Colors.black87,
+        ),
+        brightness: Brightness.dark,
+        textTheme: TextTheme(
+          body1: TextStyle(fontSize: 18),
+          body2: TextStyle(fontSize: 16),
+          button: TextStyle(letterSpacing: 1.5, fontWeight: FontWeight.bold),
+          headline: TextStyle(fontWeight: FontWeight.bold),
+          subhead: TextStyle(color: Colors.grey),
         ),
       ),
     );
   }
-}
-
-class SlideshowScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Animation'), backgroundColor: Colors.red,),
-          body: Center(
-            child: CoolBox(),
-          )
-      ),
-    );
-  }
-
-}
-
-class CoolBox extends StatefulWidget {
-  const CoolBox({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  _CoolBoxState createState() => _CoolBoxState();
-}
-
-class _CoolBoxState extends State<CoolBox> {
-
-  double width = 100;
-  double height = 100;
-  Color color = Colors.green;
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedContainer(
-        duration: Duration(seconds: 2),
-        curve: Curves.bounceInOut,
-        color: color,
-        width: width,
-        height: height,
-        child: FlatButton(
-          child: Text('Random'),
-          onPressed: () {
-
-            setState(() {
-              width = Random().nextDouble() * 400;
-              height = Random().nextDouble() * 400;
-
-              int r = Random().nextInt(255);
-              int b = Random().nextInt(255);
-              int g = Random().nextInt(255);
-              color = Color.fromRGBO(r, b, g, 1);
-            });
-
-
-          },
-        )
-    );
-  }
-
 }

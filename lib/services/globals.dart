a9b5aace1ed36bab72fa97e4e5685a8a972f8c05
Arpan Dutta <smalldutta@gmail.com>
2021-05-29 10:42:23 +0300

    makes a working app

--- lib/services/globals.dart
+++ lib/services/globals.dart
@@ -4,6 +4,11 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 
 /// Static global state. Immutable services that do not care about build context.
 class Global {
+  // App Data
+  static final String title = 'Fireship';
+
+  // Services
+  static final FirebaseAnalytics analytics = FirebaseAnalytics();
 
   // Data Models
   static final Map models = {
@@ -11,5 +16,10 @@ class Global {
     Quiz: (data) => Quiz.fromMap(data),
     Report: (data) => Report.fromMap(data),
   };
+
+  // Firestore References for Writes
+  static final Collection<Topic> topicsRef = Collection<Topic>(path: 'topics');
+  static final UserData<Report> reportRef = UserData<Report>(collection: 'reports');
+
 
 }
